5a93d2e8b66cc37489a3f00cbb1d49e63bbcb638
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-04-21 15:27:09 -0700

    Hello compose

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-export default function {{NAME}} (alpha: mixed): mixed {
-  return alpha
+import thrush from "@unction/thrush";
+import reduceValues from "@unction/reducevalues";
+
+export default function pipe (unctions) {
+  return function pipeUnctions (initial) {
+    return reduceValues(thrush)(initial)(unctions);
+  };
 }
--- test.js
+++ test.js
@@ -1,8 +1,26 @@
-/* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
+/* eslint-disable no-magic-numbers */
+import {test} from "tap";
 
-import {{NAME}} from "./index"
+import compose from "./index";
 
-test("works", () => {
-  expect({{NAME}}(true)
-    .toBe(false)
-})
+const increment = (value) => value + 1;
+const toString = (value) => `${value}`;
+const toInteger = (value) => parseInt(value, 10);
+
+test(({same, end}) => {
+  same(
+    compose([increment, increment, increment])(0),
+    3
+  );
+
+  end();
+});
+
+test(({same, end}) => {
+  same(
+    compose([toInteger, toString])(0),
+    0
+  );
+
+  end();
+});
