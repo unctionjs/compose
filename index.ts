afe6660df08def95e1b47e1434f0236b7c6ff029
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 01:17:23 -0700

    Annotating, fixing issues with packages

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@ import thrush from "@unction/thrush";
 import reduceValues from "@unction/reducevalues";
 import reverse from "@unction/reverse";
 
-export default function compose (unctions) {
-  return function composeUnctions (initial) {
+export default function compose<A, B, C> (unctions: Array<A>) {
+  return function composeUnctions (initial: B): C {
     return reduceValues(thrush)(initial)(reverse(unctions));
   };
 }
