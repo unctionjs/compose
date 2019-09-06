731186b85822846c7320967afd2177814adff154
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 14:56:45 -0700

    The transformer didnt fix this

--- test.ts
+++ test.ts
@@ -6,14 +6,14 @@ const toString = (value) => `${value}`;
 const toInteger = (value) => parseInt(value, 10);
 const append = (left) => (right) => `${right}${left}`;
 
-test(() => {
+test("works", () => {
   expect(compose([increment, increment, increment])(0)).toBe(3);
 });
 
-test(() => {
+test("works", () => {
   expect(compose([toInteger, toString])(0)).toBe(0);
 });
 
-test(() => {
+test("works", () => {
   expect(compose([append("b"), append("a")])("c")).toBe("cab");
 });
